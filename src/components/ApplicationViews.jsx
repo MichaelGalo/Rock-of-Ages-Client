aeb6ea3141bfe27fa63189a6bd22202736a8f541
Michael Galo <michael@michaelthinks.co>
2024-09-11 11:27:28 -0500

    Created My-Rocks Page

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -6,6 +6,7 @@ import Home from "../pages/Home"
 import { RockForm } from "./RockForm.jsx"
 import { RockList } from "./RockList.jsx"
 import { Register } from '../pages/Register.jsx'
+import { UserRockList } from './UserRockList.jsx'
 
 
 export const ApplicationViews = () => {
@@ -29,6 +30,17 @@ export const ApplicationViews = () => {
         setRocksState(rocks)
     }
 
+    const fetchMyRocksFromAPI = async () => {
+        const response = await fetch("http://localhost:8000/rocks?owner=current",
+            {
+                headers: {
+                    Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
+                }
+            })
+        const rocks = await response.json()
+        setRocksState(rocks)
+    }
+
     return <BrowserRouter>
         <Routes>
             <Route path="/login" element={<Login />} />
@@ -37,7 +49,7 @@ export const ApplicationViews = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/allrocks" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
                 <Route path="/create" element={<RockForm fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/mine" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
+                <Route path="/mine" element={<UserRockList rocks={rocksState} fetchMyRocksFromAPI={fetchMyRocksFromAPI} />} />
             </Route>
         </Routes>
     </BrowserRouter>
---
+++ src/components/UserRockList.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+
+export const UserRockList = ({ rocks, fetchMyRocksFromAPI }) => {
+    useEffect(() => {
+        fetchMyRocksFromAPI()
+    }, [])
+
+    const displayRocks = () => {
+        if (rocks && rocks.length) {
+            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
+                <div>{rock.name} ({rock.type.label}) weighs {rock.weight} kg.</div>
+            </div>)
+        }
+
+        return <h3>Loading Rocks...</h3>
+    }
+
+    return (
+        <>
+            <h1 className="text-3xl">Your Rock List</h1>
+            {displayRocks()}
+        </>
+    )
+}
